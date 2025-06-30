df5767bf5c2bc1190d0d075dbaffdac4bcd64c02
Caliu Pina <pinacaliu@gmail.com>
2025-06-29 23:51:32 -0300

    Correções scroll

--- frontend/src/components/root/index.js
+++ frontend/src/components/root/index.js
@@ -4,10 +4,12 @@ import React from 'react';
 import Navigation from '../navigation';
 import { Outlet } from 'react-router-dom';
 import Footer from "../footer";
+import ScrollToTop from "../scrollToTop";
 
 const Root = () => {
     return (
         <>
+            <ScrollToTop />
             <Navigation/>
             <Outlet />
             <Footer/>
